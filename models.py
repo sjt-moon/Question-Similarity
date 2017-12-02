227c5d767c7ab2ee5efb9cf9de4b6ce63af276fe
sjt-moon <harry.jt.song@gmail.com>
2017-12-02 15:05:45 -0800

    add SVM, DT, RF, KNN

--- models.py
+++ models.py
@@ -7,23 +7,10 @@ import re
 
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
+from sklearn import tree
+from sklearn.neighbors import NearestNeighbors
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
-
-# get raw text dataframe, dataframe
-raw_text = pd.read_csv('./train_x_tab.csv',sep='\t',names=['qid','question'])
-xtr, ytr, xva, yva, xte, yte = pipeline.split_dataset()
-
-# get tfidf matrix & model, array
-tf_tr, tf_va, tf_te, tfidf = pipeline.get_tfidf_vectors(xtr,xva,xte)
-
-# word2vec, dataframe
-word2vec = pipeline.get_word2vec()
-
-# raw text features, array
-feat_tr, feat_va, feat_te = pipeline.gen_raw_text_features(xtr,xva,xte,tfidf,word2vec)
-
-# sentence topics, k = 30, array
-w_tr, w_va, w_te, nmf = pipeline.generate_sentence_topics(tf_tr,tf_va,tf_te,30)
 
 # models
 
@@ -44,7 +31,7 @@ def LR(x_train,y_train,x_valid,y_valid,x_test,y_test,penalty=['l1','l2'],C=[100,
             clf_lr = LogisticRegression(C=c, penalty=p, tol=tol)
             clf_lr.fit(x_train, y_train)
             score = clf_lr.score(x_valid, y_valid)
-            if not best_model or score>best_score:
+            if not best_model or score>best_accuracy:
                 best_model = clf_lr
                 best_accuracy = score
     print('Best LR model is:')
@@ -73,6 +60,50 @@ def SVM(x_train,y_train,x_valid,y_valid,x_test,y_test,C=[100,1,0.01]):
             best_accuracy = score
     print('Best SVM model is:')
     print(best_model.get_params())
+    print('Best score on validation set: {}'.format(best_accuracy))
+    print('Accuracy on test set: {}'.format(best_model.score(x_test, y_test)))
+    return best_model
+
+# Desicion trees
+def DT(x_train,y_train,x_valid,y_valid,x_test,y_test):
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(x_train, y_train)
+    score = clf.score(x_valid, y_valid)
+    print('Score on validation set: {}'.format(score))
+    print('Accuracy on test set: {}'.format(clf.score(x_test, y_test)))
+    return clf
+
+# kNN
+def kNN(x_train,y_train,x_valid,y_valid,x_test,y_test,n_neighbors=[5,10,20,50]):
+    best_accuracy = -9999
+    best_model = None
+    for k in n_neighbors:
+        clf = neighbors.KNeighborsClassifier(n_neighbors=k)
+        clf.fit(x_train, y_train)
+        score = clf.score(x_valid, y_valid)
+        if not best_model or score>best_accuracy:
+            best_model = clf
+            best_accuracy = score
+    print('Best kNN model is:')
+    print(best_model.get_params())
+    print('Best score on validation set: {}'.format(best_accuracy))
+    print('Accuracy on test set: {}'.format(best_model.score(x_test, y_test)))
+    return best_model
+
+# Random forest
+def RF(x_train,y_train,x_valid,y_valid,x_test,y_test,n_estimators=[]):
+    best_accuracy = -9999
+    best_model = None
+    for k in n_estimators:
+        clf = RandomForestClassifier(n_estimators=k)
+        clf.fit(x_train, y_train)
+        score = clf.score(x_valid, y_valid)
+        if not best_model or score>best_accuracy:
+            best_model = clf
+            best_accuracy = score
+    print('Best kNN model is of {} estimators'.format(best_model.estimators_))
+    print('Feature importance')
+    print(best_model.feature_importances_)
     print('Best score on validation set: {}'.format(best_accuracy))
     print('Accuracy on test set: {}'.format(best_model.score(x_test, y_test)))
     return best_model
