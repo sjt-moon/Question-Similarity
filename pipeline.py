5f4c75769cb1bb1c332d3e7606891dc630a4d7c2
sjt-moon <harry.jt.song@gmail.com>
2017-12-02 15:00:12 -0800

    fix some typos

--- pipeline.py
+++ pipeline.py
@@ -19,6 +19,12 @@ def split_dataset(fx="train_x_tab.csv", fy="train_y_tab.csv", ratios = [0.6,0.2,
 
     x_tr,x_va,x_te = X[:2*valid_start_index],X[2*valid_start_index:2*test_start_index],X[2*test_start_index:]
     y_tr, y_va, y_te = y[:valid_start_index],y[valid_start_index:test_start_index],y[test_start_index:]
+
+    # change row indices, start from 0
+    x_va.index = range(len(x_va))
+    y_va.index = range(len(y_va))
+    x_te.index = range(len(x_te))
+    y_te.index = range(len(y_te))
     
     assert len(x_tr) == 2 * len(y_tr)
     assert len(x_va) == 2 * len(y_va)
@@ -56,17 +62,14 @@ def gen_raw_feat(X, tfidf, word2vec, filename):
     #feat = []
     with open(filename,'w') as fw:
 
-        col_names = ['num_chars_q1','num_chars_q2','num_terms_q1','num_terms_q2','common_sz','union_sz']
-        col_names += ['sen_product',]
+        col_names = ['diff_num_chars','diff_num_terms','common_ratio','sen_product',]
         fw.write('\t'.join(col_names)+'\n')
 
         sz = len(X)
         print(sz)
         i = 0
         while i<sz:
-            if i%10==0:
-                print(i)
-            if i%(int(sz/20))==0:
+            if i%(int(sz/100))==0:
                 print('{}% complete'.format(int(i*100/sz)))
 
             q1, q2 = X['question'][i], X['question'][i+1]
@@ -86,26 +89,25 @@ def gen_raw_feat(X, tfidf, word2vec, filename):
     
             num_chars_q1 = sum(len(w) for w in q1)
             num_chars_q2 = sum(len(w) for w in q2)
+            diff_num_chars = abs(num_chars_q1 - num_chars_q2)
 
             num_terms_q1, num_terms_q2 = len(q1), len(q2)
-            common_sz, union_sz = len(common), len(union)
+            diff_num_terms = abs(num_terms_q1 - num_terms_q2)
 
-            # same with num_terms
-            #r_common_q1 = common_sz / num_terms_q1
-            #r_common_q2 = common_sz / num_terms_q2
+            common_sz, union_sz = len(common), len(union)
+            common_ratio = 1.0 * common_sz / union_sz
 
             # sentence2vec 
             v1 = sentence2vec(q1, tfidf, word2vec)
             v2 = sentence2vec(q2, tfidf, word2vec)
             sen_product = v1.dot(v2)
 
-            line=[num_chars_q1,num_chars_q2,num_terms_q1,num_terms_q2,common_sz,union_sz,sen_product]
+            line=[diff_num_chars, diff_num_terms, common_ratio, sen_product]
             line = [str(ele) for ele in line]
             #feat.append(line)
             fw.write('\t'.join(line)+'\n')
 
             i += 2
-        print('{} pairs processed'.format(len(feat)))
     return
 
 def get_word2vec():
@@ -117,6 +119,7 @@ def get_word2vec():
         for line in fr.readlines():
             keys_embeddings.append(line.strip().lower())
     word2vec['word'] = keys_embeddings
+    word2vec.set_index('word')
     return word2vec
 
 def get_word2vec_question(word2vec, question, max_terms_per_question):
@@ -229,6 +232,33 @@ def generate_sentence_topics(X_train, X_valid, X_test, k):
 
     return W_train, W_valid, W_test, nmf_model, col_names
 
+def to_pairwise_cosine(x,col_name=['cos_sim']):
+    '''x[i], x[i+1] -> cosine(x[i], x[i+1])
+    
+    @para
+    x: sparse matrix or np.array
+    '''
+    import warnings
+    warnings.filterwarnings("ignore")
+
+    is_sparse = sp.sparse.issparse(X)
+    
+    ans = []
+    i = 0
+    sz = x.shape[0]
+    while i<sz:
+        if i%int(sz/10)==0:
+            print('{}%'.format(i*100/sz))
+        if is_sparse:
+            v1 = np.array(x[i].todense())[0]
+            v1 = np.array(x[i].todense())[0]
+            sim = cosine_similarity(v1, v2)[0][0]
+        else:
+            sim = cosine_similarity(x[i], x[i+1])[0][0]
+        ans.append(sim)
+        i += 2
+    return pd.DataFrame(np.array(ans), columns=col_name)
+
 def to_dataframe(X, col_names, filename=None):
     assert col_names and len(col_names)>0
     if sp.sparse.issparse(X):
@@ -265,20 +295,6 @@ def _sparse_to_dataframe_(X, col_names, filename='nmf_tr.csv'):
             fw.write(line)
             i += 1
     print('Now get dataframes with pd.read_csv for {}'.format(filename))
-
-def to_pairwise_dataframe(X):
-    '''Add some pairwise features.
-    
-    NOTE:
-    X[2i] & X[2i+1] -> a pair, y[i]
-    thus len(X) = 2 * len(y)
-    '''
-    # col names?
-    # what about sparse matrix?
-    sz, num_dim = len(X), len(X[0])
-    assert sz%2==0
-    X = np.array(X).reshape((int(sz/2), int(num_dim*2)))
-    return pd.DataFrame(X)
 
 def main():
     # This is an example for work flow
