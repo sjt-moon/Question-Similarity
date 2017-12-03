46d7adf236a1606e4081db7cae7e9aeb16a79ed1
sjt-moon <harry.jt.song@gmail.com>
2017-12-03 10:36:07 -0800

    standarlized baseline input

--- baseline.py
+++ baseline.py
@@ -16,7 +16,13 @@ def cosine_similarity_model(X, y):
     '''There is no need to train anything. Use all of the data.
     
     Note that len(X) = 2*len(y)
+
+    @para
+    X: sparse matrices, tfdif matrices
+    y: dataframe
     '''
+    assert sp.sparse.issparse(X)
+    assert X.shape[0] == 2*len(y)
     print('calculating cosine similarity')
 
     correct, wrong = 0, 0
@@ -25,13 +31,13 @@ def cosine_similarity_model(X, y):
     while (i<sz):
         if (i%(int(sz/10))==0):
             print("{}% training set completed".format(i*100/sz))
-        sim = cosine_similarity(X[i], X[i+1])[0][0]
+        sim = cosine_similarity(np.array(X[i].todense())[0], np.array(X[i+1].todense())[0])[0][0]
         pred = 1 if sim>=0.5 else 0
-        if pred==y[int(i/2)]:
+        if pred==int(y.loc[int(i/2)]):
             correct += 1
         else:
             wrong += 1
-        i += 1
+        i += 2
     print("#correct predictions:\t{}\n#wrong answers:\t{}\naccuracy:\t{}".format(correct, wrong, correct/(correct+wrong)))
 
 def main():
