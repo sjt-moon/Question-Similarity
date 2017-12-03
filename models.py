d80f695fdd31e80da0ed00116a38920f2bff632e
sjt-moon <harry.jt.song@gmail.com>
2017-12-02 16:53:18 -0800

    add GBDT, Adaboost

--- models.py
+++ models.py
@@ -8,9 +8,15 @@ import re
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 from sklearn import tree
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import AdaBoostClassifier
 import matplotlib.pyplot as plt
+
+
+import warnings
+warnings.filterwarnings("ignore")
 
 # models
 
@@ -78,7 +84,7 @@ def kNN(x_train,y_train,x_valid,y_valid,x_test,y_test,n_neighbors=[5,10,20,50]):
     best_accuracy = -9999
     best_model = None
     for k in n_neighbors:
-        clf = neighbors.KNeighborsClassifier(n_neighbors=k)
+        clf = KNeighborsClassifier(n_neighbors=k)
         clf.fit(x_train, y_train)
         score = clf.score(x_valid, y_valid)
         if not best_model or score>best_accuracy:
@@ -91,7 +97,7 @@ def kNN(x_train,y_train,x_valid,y_valid,x_test,y_test,n_neighbors=[5,10,20,50]):
     return best_model
 
 # Random forest
-def RF(x_train,y_train,x_valid,y_valid,x_test,y_test,n_estimators=[]):
+def RF(x_train,y_train,x_valid,y_valid,x_test,y_test,n_estimators=[30,40,50,60,70,80,100]):
     best_accuracy = -9999
     best_model = None
     for k in n_estimators:
@@ -101,9 +107,52 @@ def RF(x_train,y_train,x_valid,y_valid,x_test,y_test,n_estimators=[]):
         if not best_model or score>best_accuracy:
             best_model = clf
             best_accuracy = score
-    print('Best kNN model is of {} estimators'.format(best_model.estimators_))
+    #print('Best kNN model is of {} estimators'.format(best_model.estimators_))
     print('Feature importance')
     print(best_model.feature_importances_)
     print('Best score on validation set: {}'.format(best_accuracy))
     print('Accuracy on test set: {}'.format(best_model.score(x_test, y_test)))
     return best_model
+
+# GBDT
+def GBDT(x_train,y_train,x_valid,y_valid,x_test,y_test,n_estimators=[100,200,300],depths=[1,3,5],lrs=[0.1,0.5,1.0]):
+    best_accuracy = -9999
+    best_model = None
+    for k in n_estimators:
+        for d in depths:
+            for lr in lrs:
+                clf = GradientBoostingClassifier(n_estimators=100,learning_rate=lr,max_depth=d)
+                clf.fit(x_train, y_train)
+                score = clf.score(x_valid, y_valid)
+                if not best_model or score>best_accuracy:
+                    best_model = clf
+                    best_accuracy = score
+    #print('Best GBDT model is of {} estimators'.format(best_model.estimators_))
+    print('Feature importance')
+    print(best_model.feature_importances_)
+    print('Best score on validation set: {}'.format(best_accuracy))
+    print('Accuracy on test set: {}'.format(best_model.score(x_test, y_test)))
+    return best_model
+
+# Adaboost
+def Adaboost(x_train,y_train,x_valid,y_valid,x_test,y_test,n_estimators=[100,200,300],lrs=[0.1,0.5,1.0]):
+    best_accuracy = -9999
+    best_model = None
+    for k in n_estimators:
+        for lr in lrs:
+            clf = AdaBoostClassifier(n_estimators=k, learning_rate=lr)
+            clf.fit(x_train, y_train)
+            score = clf.score(x_valid, y_valid)
+            if not best_model or score>best_accuracy:
+                best_model = clf
+                best_accuracy = score
+    #print('Best GBDT model is of {} estimators'.format(best_model.estimators_))
+    print('Feature importance')
+    print(best_model.feature_importances_)
+    print('Best score on validation set: {}'.format(best_accuracy))
+    print('Accuracy on test set: {}'.format(best_model.score(x_test, y_test)))
+    return best_model
+
+
+
+
